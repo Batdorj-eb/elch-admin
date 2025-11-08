5c03aae15f6fb1a713814c9223b17dcb7b1178ed
Babuna <74554961+Babuna-0501@users.noreply.github.com>
2025-11-08 20:51:14 +0800

    fix d

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,235 +1,217 @@
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import {
-  ClassicEditor,
-  AccessibilityHelp,
-  Alignment,
-  AutoLink,
-  Autosave,
-  Bold,
-  BlockQuote,
-  Code,
-  Essentials,
-  FontBackgroundColor,
-  FontColor,
-  FontFamily,
-  FontSize,
-  Heading,
-  Highlight,
-  HorizontalLine,
-  Italic,
-  Link,
-  List,
-  Paragraph,
-  PasteFromOffice,
-  RemoveFormat,
-  SpecialCharacters,
-  SpecialCharactersEssentials,
-  Strikethrough,
-  Subscript,
-  Superscript,
-  Table,
-  TableToolbar,
-  Underline,
-  Undo,
-  ImageBlock,
-  ImageCaption,
-  ImageInline,
-  ImageInsert,
-  ImageResize,
-  ImageStyle,
-  ImageToolbar,
-  ImageUpload,
-  MediaEmbed,
-  Indent,
-  IndentBlock,
-} from 'ckeditor5';
-
-import 'ckeditor5/ckeditor5.css';
-
-// Custom Upload Adapter
-class MyUploadAdapter {
-  constructor(loader, uploadFunction) {
-    this.loader = loader;
-    this.uploadFunction = uploadFunction;
-  }
-
-  upload() {
-    return this.loader.file.then(file => this.uploadFunction(file));
-  }
-
-  abort() {}
-}
-
-function MyCustomUploadAdapterPlugin(editor, uploadFunction) {
-  editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
-    return new MyUploadAdapter(loader, uploadFunction);
-  };
-}
+import { useEffect, useRef } from 'react';
 
 export default function Editor({ value, onChange, placeholder = "Контент бичих...", uploadImage }) {
-  const editorConfig = {
-    licenseKey: 'GPL', // 🔥 GPL License (үнэгүй)
-    toolbar: {
-      items: [
-        'undo',
-        'redo',
-        '|',
-        'heading',
-        '|',
-        'fontSize',
-        'fontFamily',
-        'fontColor',
-        'fontBackgroundColor',
-        '|',
-        'bold',
-        'italic',
-        'underline',
-        'strikethrough',
-        '|',
-        'subscript',
-        'superscript',
-        'code',
-        '|',
-        'link',
-        'insertImage',
-        'mediaEmbed',
-        'insertTable',
-        'blockQuote',
-        'horizontalLine',
-        '|',
-        'alignment',
-        '|',
-        'bulletedList',
-        'numberedList',
-        '|',
-        'outdent',
-        'indent',
-        '|',
-        'highlight',
-        'specialCharacters',
-        'removeFormat',
-      ],
-      shouldNotGroupWhenFull: false,
-    },
-    plugins: [
-      AccessibilityHelp,
-      Alignment,
-      AutoLink,
-      Autosave,
-      Bold,
-      BlockQuote,
-      Code,
+  const editorRef = useRef(null);
+  const instanceRef = useRef(null);
+  const isInitializedRef = useRef(false);
+
+  useEffect(() => {
+    // Load CKEditor from CDN
+    if (!window.CKEDITOR) {
+      const script = document.createElement('script');
+      script.src = 'https://cdn.ckeditor.com/ckeditor5/43.3.1/ckeditor5.umd.js';
+      script.async = true;
+      script.onload = () => initEditor();
+      document.body.appendChild(script);
+
+      const link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.href = 'https://cdn.ckeditor.com/ckeditor5/43.3.1/ckeditor5.css';
+      document.head.appendChild(link);
+    } else {
+      initEditor();
+    }
+
+    return () => {
+      if (instanceRef.current) {
+        instanceRef.current.destroy().catch(err => console.error(err));
+        instanceRef.current = null;
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (instanceRef.current && value !== instanceRef.current.getData()) {
+      instanceRef.current.setData(value || '');
+    }
+  }, [value]);
+
+  const initEditor = () => {
+    if (!editorRef.current || instanceRef.current || isInitializedRef.current) return;
+    if (!window.CKEDITOR) return;
+
+    isInitializedRef.current = true;
+
+    const {
+      ClassicEditor,
       Essentials,
-      FontBackgroundColor,
-      FontColor,
-      FontFamily,
-      FontSize,
-      Heading,
-      Highlight,
-      HorizontalLine,
+      Bold,
       Italic,
-      Link,
-      List,
-      Paragraph,
-      PasteFromOffice,
-      RemoveFormat,
-      SpecialCharacters,
-      SpecialCharactersEssentials,
+      Underline,
       Strikethrough,
       Subscript,
       Superscript,
-      Table,
-      TableToolbar,
-      Underline,
-      Undo,
-      ImageBlock,
+      Code,
+      Paragraph,
+      Heading,
+      Link,
+      List,
+      BlockQuote,
+      Image,
       ImageCaption,
-      ImageInline,
-      ImageInsert,
-      ImageResize,
       ImageStyle,
       ImageToolbar,
       ImageUpload,
+      ImageResize,
       MediaEmbed,
+      Table,
+      TableToolbar,
+      Alignment,
+      Undo,
+      FontFamily,
+      FontSize,
+      FontColor,
+      FontBackgroundColor,
+      Highlight,
+      RemoveFormat,
+      HorizontalLine,
+      SpecialCharacters,
+      SpecialCharactersEssentials,
+      PasteFromOffice,
       Indent,
       IndentBlock,
-    ],
-    heading: {
-      options: [
-        {
-          model: 'paragraph',
-          title: 'Paragraph',
-          class: 'ck-heading_paragraph',
+    } = window.CKEDITOR;
+
+    // Custom Upload Adapter
+    class UploadAdapter {
+      constructor(loader) {
+        this.loader = loader;
+      }
+
+      upload() {
+        return this.loader.file.then(file => uploadImage(file));
+      }
+
+      abort() {}
+    }
+
+    function UploadAdapterPlugin(editor) {
+      editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+        return new UploadAdapter(loader);
+      };
+    }
+
+    ClassicEditor
+      .create(editorRef.current, {
+        licenseKey: 'GPL',
+        plugins: [
+          Essentials,
+          Bold,
+          Italic,
+          Underline,
+          Strikethrough,
+          Subscript,
+          Superscript,
+          Code,
+          Paragraph,
+          Heading,
+          Link,
+          List,
+          BlockQuote,
+          Image,
+          ImageCaption,
+          ImageStyle,
+          ImageToolbar,
+          ImageUpload,
+          ImageResize,
+          MediaEmbed,
+          Table,
+          TableToolbar,
+          Alignment,
+          Undo,
+          FontFamily,
+          FontSize,
+          FontColor,
+          FontBackgroundColor,
+          Highlight,
+          RemoveFormat,
+          HorizontalLine,
+          SpecialCharacters,
+          SpecialCharactersEssentials,
+          PasteFromOffice,
+          Indent,
+          IndentBlock,
+        ],
+        toolbar: [
+          'undo', 'redo',
+          '|',
+          'heading',
+          '|',
+          'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor',
+          '|',
+          'bold', 'italic', 'underline', 'strikethrough',
+          '|',
+          'subscript', 'superscript', 'code',
+          '|',
+          'link', 'uploadImage', 'mediaEmbed', 'blockQuote',
+          '|',
+          'alignment',
+          '|',
+          'bulletedList', 'numberedList',
+          '|',
+          'outdent', 'indent',
+          '|',
+          'insertTable', 'horizontalLine', 'specialCharacters',
+          '|',
+          'highlight', 'removeFormat',
+        ],
+        heading: {
+          options: [
+            { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+            { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+            { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+            { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+            { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+            { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
+            { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' },
+          ],
         },
-        {
-          model: 'heading1',
-          view: 'h1',
-          title: 'Heading 1',
-          class: 'ck-heading_heading1',
+        image: {
+          toolbar: [
+            'imageTextAlternative',
+            '|',
+            'imageStyle:inline',
+            'imageStyle:wrapText',
+            'imageStyle:breakText',
+            '|',
+            'resizeImage',
+          ],
         },
-        {
-          model: 'heading2',
-          view: 'h2',
-          title: 'Heading 2',
-          class: 'ck-heading_heading2',
+        table: {
+          contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells'],
         },
-        {
-          model: 'heading3',
-          view: 'h3',
-          title: 'Heading 3',
-          class: 'ck-heading_heading3',
-        },
-        {
-          model: 'heading4',
-          view: 'h4',
-          title: 'Heading 4',
-          class: 'ck-heading_heading4',
-        },
-        {
-          model: 'heading5',
-          view: 'h5',
-          title: 'Heading 5',
-          class: 'ck-heading_heading5',
-        },
-        {
-          model: 'heading6',
-          view: 'h6',
-          title: 'Heading 6',
-          class: 'ck-heading_heading6',
-        },
-      ],
-    },
-    image: {
-      toolbar: [
-        'imageTextAlternative',
-        '|',
-        'imageStyle:inline',
-        'imageStyle:wrapText',
-        'imageStyle:breakText',
-        '|',
-        'resizeImage',
-      ],
-    },
-    table: {
-      contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells'],
-    },
-    placeholder: placeholder,
-    extraPlugins: uploadImage ? [
-      (editor) => MyCustomUploadAdapterPlugin(editor, uploadImage)
-    ] : [],
+        placeholder: placeholder,
+        extraPlugins: uploadImage ? [UploadAdapterPlugin] : [],
+      })
+      .then(editor => {
+        instanceRef.current = editor;
+        editor.setData(value || '');
+        editor.model.document.on('change:data', () => {
+          const data = editor.getData();
+          onChange(data);
+        });
+      })
+      .catch(error => {
+        console.error('CKEditor initialization error:', error);
+        isInitializedRef.current = false;
+      });
   };
 
   return (
-    <div className="editor-container">
-      <CKEditor
-        editor={ClassicEditor}
-        config={editorConfig}
-        data={value || ''}
-        onChange={(event, editor) => {
-          const data = editor.getData();
-          onChange(data);
-        }}
-      />
-    </div>
+    <div 
+      ref={editorRef} 
+      className="editor-container"
+      style={{ minHeight: '400px' }}
+    />
   );
 }
